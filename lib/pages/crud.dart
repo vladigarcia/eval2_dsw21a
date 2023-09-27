949a0637bf37b2d1837c424e9c35d8c067c580e9
vladigarcia <144178838+vladigarcia@users.noreply.github.com>
2023-09-27 10:42:45 -0600

    cambios

--- lib/pages/crud.dart
+++ lib/pages/crud.dart
@@ -1,42 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+FirebaseFirestore db = FirebaseFirestore.instance;
 
-  final String title;
+// Metodo para consultar toda la información de la colección tb_productos
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+Future<lits>
 
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:() {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
