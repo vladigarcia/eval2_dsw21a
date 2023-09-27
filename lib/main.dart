ddd16250970f1f1286fa47172f61e6b73c55cccd
jonathanenmanuelpalacioscarran@gmail.com <jairovespon@gmail.com>
2023-09-27 09:36:53 -0600

    cambio
    nombre definitivo a la app

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'examen practico'),
+      home: const MyHomePage(title: ' Sistema de Inventariado | Eval2'),
     );
   }
 }
